938cb66e2dc98bafb9606e46e211c79c1819958a
Burhanul <burhanulwdd@gmail.com>
2025-02-23 22:51:28 +0600

    Added how to addMoney in account balanace

--- js/home.js
+++ js/home.js
@@ -1,14 +1,36 @@
 // add  money to the amount
 // step-1: Add an event handler to the Add Money button inside the form
-document.getElementById('btn-add-money').addEventListener('click', function(event){
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
     // prevent page reload after form submit
     event.preventDefault();
-    
+
     // step-2: get money to be added to the account
     const addMoneyInput = document.getElementById('input-add-money').value;
     console.log(addMoneyInput);
 
     // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput);
+    console.log(typeof pinNumberInput);
+
+    // step-3: verify pin number
+    // wrong way to validate pin number
+    if (pinNumberInput === '1234') {
+        console.log('adding money to your account');
+
+        // step-4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(typeof balance);
+
+        // step-5: add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step-6: update the new balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+       alert('Faild to add money! Please try again.')
+    }
 })
